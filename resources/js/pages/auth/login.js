e82915ab1fe84f80aeb853be390a99e7f4fd70d4
T0mBPok <shelter.play.akk@mail.ru>
2023-08-07 14:29:16 +0300

    Пробная версия

--- resources/js/pages/auth/login.js
+++ resources/js/pages/auth/login.js
@@ -1,1 +1,16 @@
 console.log(1);
+
+
+const l_characters = document.getElementById('l_characters');
+const l_character = l_characters.querySelectorAll('.login__character')
+
+for (var i=0; i<l_character.length; i++){
+    l_character[i].addEventListener('click', function(evt){
+        evt.preventDefault();
+        l_character.forEach(c => {
+            c.classList.remove('btn-active');
+        })
+        $(this).addClass('btn-active');
+    })
+}
+
