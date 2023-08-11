74d94c520afc1fb7ead061ac6a219a213b1017ce
T0mBPok <shelter.play.akk@mail.ru>
2023-08-11 18:02:01 +0300

    new file:   resources/js/pages/employees/search.js 	modified:   resources/sass/pages/home.sass 	modified:   resources/views/pages/home/employees.blade.php

---
+++ resources/js/pages/employees/search.js
@@ -0,0 +1,16 @@
+function search(){
+	const input = document.querySelector('.employees__search')
+	let filter = input.value.toLowerCase().replaceAll(' ', '');
+	const students = document.querySelectorAll('.empoyees__name')
+	students.forEach(student => {
+		let [firstName, lastName, patronymic] = student.textContent.split(' ')
+		let fullName = `${firstName}${lastName}${patronymic}`.toLowerCase()
+		if (fullName.indexOf(filter) > -1) {
+			student.parentNode.style.display = ''
+		} else {
+			student.parentNode.style.display = 'none'
+		}
+	});
+}
+
+document.addEventListener('input', search)
--- resources/views/pages/home/employees.blade.php
+++ resources/views/pages/home/employees.blade.php
@@ -13,103 +13,111 @@ $array = array_map('str_getcsv', $csv);
 
 @stop
 @section("content")
-	<h1>Список сотрудников</h1>
 	<article class="employees">
-		<ul class="employees__list">
-			<?php
-			$i = 0;
-			foreach ($array as $item) {
-				$i++;
-				$id = $i - 1;
-				$html = "<li data-id='$id'>";
-				$html .= '<span>'.$i.'</span>';
-				foreach ($item as $value) {
-					$html .= '<span>'.$value.'</span>';
+		<div class="employees__header">
+			<input
+			type="text"
+			class="employees__search input__primary"
+			placeholder="Введите ФИО сотрудника"
+			/>
+		</div>
+		<div class="employees__main">
+			<ul class="employees__list">
+				<?php
+				$i = 0;
+				foreach ($array as $item) {
+					$i++;
+					$id = $i - 1;
+					$html = "<li data-id='$id'>";
+					$html .= '<span>'.$i.'</span>';
+					foreach ($item as $value) {
+						$html .= "<span class='empoyees__name'>".$value."</span>";
+					}
+					$html .= '</li>';
+					echo $html;
 				}
-				$html .= '</li>';
-				echo $html;
-			}
-			?>
-		</ul>
-		<article class="modal hidden" id="employee-modal">
-			<div class="modal__content" id="employee-modal-content">
-				<div class="card">
-					<div class="card__header">
-						<button class="btn__primary card__header-btn active">
-							Характеристика от руководителя
-						</button>
-						<button class="btn__primary card__header-btn">
-							Итоги встречи
-						</button>
-						<button class="btn__primary card__header-btn">
-							Итоги предыдущей встречи
-						</button>
+				?>
+			</ul>
+			<article class="modal hidden" id="employee-modal">
+				<div class="modal__content" id="employee-modal-content">
+					<div class="card">
+						<div class="card__header">
+							<button class="btn__primary card__header-btn active">
+								Характеристика от руководителя
+							</button>
+							<button class="btn__primary card__header-btn">
+								Итоги встречи
+							</button>
+							<button class="btn__primary card__header-btn">
+								Итоги предыдущей встречи
+							</button>
+						</div>
+						<ul class="card__content">
+							<li class="card__content-item" id="descriptionFromDirector">
+								<form class="card__content-item-form" id="directorForm">
+									<label>
+										Дата заполнения (чч.мм.гг.):
+										<input class="input__primary" type="date" name="dateOfWrite" />
+									</label>
+									<label>
+										ФИО студента:
+										<input class="input__primary" type="text" name="fullNameStudent" value="Яковлев Алексей Николаевич" />
+									</label>
+									<label>
+										Структурное подразделение (Департамент):
+										<select name="departament">
+											<option value="IT">IT</option>
+											<option value="Design">Дизайн</option>
+											<option value="Manager">Управление</option>
+										</select>
+									</label>
+									<label>
+										Занимаемая студентом должность:
+										<input class="input__primary" type="text" name="post" />
+									</label>
+									<label>
+										Период работы на данной позиции:
+										<input class="input__primary" type="text" name="periodOfWork" />
+									</label>
+									<label>
+										Основные достижения за время работы:
+										<input class="input__primary" type="text" name="achivmentsForWork" />
+									</label>
+									<label>
+										Сильные стороны и развитые компетенции:
+										<input class="input__primary" type="text" name="strongSides" />
+									</label>
+									<label>
+										Замечания и упущения в работе:
+										<input class="input__primary" type="text" name="commentsOnWork" />
+									</label>
+									<label>
+										Проявленные личные качества:
+										<input class="input__primary" type="text" name="personalQualities" />
+									</label>
+									<label>
+										Зоны ближайшего развития:
+										<input class="input__primary" type="text" name="developmentSides" />
+									</label>
+									<label>
+										Компетенции, которые необходимо развить в среднесрочной перспективе:
+										<input class="input__primary" type="text" name="developCompetencies" />
+									</label>
+									<label>
+										Карьерные и экспертные перспективы:
+										<input class="input__primary" type="text" name="careerProspects" />
+									</label>
+									<button class="btn__primary">Прикрепить</button>
+								</form>
+							</li>
+							<li class="card__content-item hidden" id="resultsOfMeeting">
+								<p>Здесь должна быть таблица</p>
+							</li>
+							<li class="card__content-item hidden" id="resultsOfPrevMeeting">
+								<p>Здесь должна быть таблица</p>
+							</li>
+						</ul>
 					</div>
-					<ul class="card__content">
-						<li class="card__content-item" id="descriptionFromDirector">
-							<form class="card__content-item-form" id="directorForm">
-								<label>
-									Дата заполнения (чч.мм.гг.):
-									<input class="input__primary" type="date" name="dateOfWrite" />
-								</label>
-								<label>
-									ФИО студента:
-									<input class="input__primary" type="text" name="fullNameStudent" value="Яковлев Алексей Николаевич" />
-								</label>
-								<label>
-									Структурное подразделение (Департамент):
-									<select name="departament">
-										<option value="IT">IT</option>
-										<option value="Design">Дизайн</option>
-										<option value="Manager">Управление</option>
-									</select>
-								</label>
-								<label>
-									Занимаемая студентом должность:
-									<input class="input__primary" type="text" name="post" />
-								</label>
-								<label>
-									Период работы на данной позиции:
-									<input class="input__primary" type="text" name="periodOfWork" />
-								</label>
-								<label>
-									Основные достижения за время работы:
-									<input class="input__primary" type="text" name="achivmentsForWork" />
-								</label>
-								<label>
-									Сильные стороны и развитые компетенции:
-									<input class="input__primary" type="text" name="strongSides" />
-								</label>
-								<label>
-									Замечания и упущения в работе:
-									<input class="input__primary" type="text" name="commentsOnWork" />
-								</label>
-								<label>
-									Проявленные личные качества:
-									<input class="input__primary" type="text" name="personalQualities" />
-								</label>
-								<label>
-									Зоны ближайшего развития:
-									<input class="input__primary" type="text" name="developmentSides" />
-								</label>
-								<label>
-									Компетенции, которые необходимо развить в среднесрочной перспективе:
-									<input class="input__primary" type="text" name="developCompetencies" />
-								</label>
-								<label>
-									Карьерные и экспертные перспективы:
-									<input class="input__primary" type="text" name="careerProspects" />
-								</label>
-								<button class="btn__primary">Прикрепить</button>
-							</form>
-						</li>
-						<li class="card__content-item hidden" id="resultsOfMeeting">
-							<p>Здесь должна быть таблица</p>
-						</li>
-						<li class="card__content-item hidden" id="resultsOfPrevMeeting">
-							<p>Здесь должна быть таблица</p>
-						</li>
-					</ul>
 				</div>
 			</div>
 		</article>
@@ -117,6 +125,7 @@ $array = array_map('str_getcsv', $csv);
 @stop
 @section("scripts")
 	@vite([
+		"resources/js/pages/employees/search.js",
 		"resources/js/pages/employees/modal.js",
 		"resources/js/pages/employees/tabs.js"
 	])
