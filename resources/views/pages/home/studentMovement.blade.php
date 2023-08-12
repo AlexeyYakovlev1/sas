4d3d59d4fe81c8ba51c4ced1423ce773e18d5ebf
T0mBPok <shelter.play.akk@mail.ru>
2023-08-12 17:32:25 +0300

    modified:   resources/sass/pages/home.css 	modified:   resources/sass/pages/home.css.map modified:   resources/sass/pages/home.sass 	modified:   resources/views/pages/home/studentMovement.blade.php

--- resources/views/pages/home/studentMovement.blade.php
+++ resources/views/pages/home/studentMovement.blade.php
@@ -1,12 +1,61 @@
+<?php
+$id = '1H5qfLCQI1Lig2sQJjZQmA2s6i0BPudcHbkajIbtLI3w';
+$gid = 0;
+$csv = file_get_contents('https://docs.google.com/spreadsheets/d/'.$id.'/export?format=csv&gid='.$gid);
+$csv = explode("\r\n", $csv);
+$array = array_map('str_getcsv', $csv);
+?>
 @extends("layouts.home")
 @section("title")
 	Движение студентов
 @stop
 @section("styles")
-	
+
 @stop
 @section("content")
-	<h1>Таблица определенного формата, которая будет автомaтически обновляться</h1>
+	<div class="studentMovement">
+		<div class="studentMovement__sheet">
+			<table>
+				<thead>
+					<tr>
+						<td colspan="10"><h2>Отчет о движении Кадрового резерва(НТБ).</h2></td>
+					</tr>
+					<tr>
+						<td></td>
+						<td colspan="3">На 27.09</td>
+						<td colspan="3">Прибыло/Выбыло</td>
+						<td colspan="3">На 4.10</td>
+					</tr>
+					<tr>
+						<td></td>
+						<td>М </td>
+						<td>Ж </td>
+						<td>Всего</td>
+						<td>М </td>
+						<td>Ж</td>
+						<td>Всего</td>
+						<td>М</td>
+						<td>Ж</td>
+						<td>Всего</td>
+					</tr>
+				</thead>
+				<tbody>
+					<?php
+					$i = 0;
+						foreach ($array as $item) {
+						$i++;
+						$html = '<tr>';
+						foreach ($item as $value) {
+								$html .= '<td scope="row">'.$value.'</td>';
+							}
+						$html .= '</tr>';
+						echo $html;
+						}
+					?>
+				</tbody>
+			</table>
+		</div>
+	</div>
 @stop
 @section("scripts")
 
