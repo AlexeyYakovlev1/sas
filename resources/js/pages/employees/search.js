a5dcc6510603aa850cf330495507a79187d189b6
AlexeyYakovlev1 <alexeyykovlev555@gmail.com>
2023-08-16 11:52:57 +0300

    refactoring

--- resources/js/pages/employees/search.js
+++ resources/js/pages/employees/search.js
@@ -1,18 +1,20 @@
 "use strict";
 
+const input = document.querySelector('.employees__search');
+
 function search() {
-	const input = document.querySelector('.employees__search')
-	let filter = input.value.toLowerCase().replaceAll(' ', '');
-	const students = document.querySelectorAll('.empoyees__name')
+	if (input.value.length < 3) return;
+
+	const filter = input.value.toLowerCase().trim();
+	const students = document.querySelectorAll('.empoyees__name');
+
 	students.forEach(student => {
-		let [firstName, lastName, patronymic] = student.textContent.split(' ')
-		let fullName = `${firstName}${lastName}${patronymic}`.toLowerCase()
-		if (fullName.indexOf(filter) > -1) {
-			student.parentNode.style.display = ''
-		} else {
-			student.parentNode.style.display = 'none'
-		}
+		const [firstName, lastName, patronymic] = student.textContent.split(' ');
+		const fullName = `${firstName}${lastName}${patronymic}`.toLowerCase();
+
+		if (fullName.indexOf(filter) !== -1) student.parentNode.style.display = '';
+		else student.parentNode.style.display = 'none';
 	});
 }
 
-document.addEventListener('input', search)
+input.addEventListener('change', search);
